50ccee9b2fc1bc9020bce0f51d27dc443a5d9f48
lanpangzi-zkg <1090140795@qq.com>
2021-05-31 18:13:42 +0800

    2 files modified

--- src/mini-react-v2/react-dom.js
+++ src/mini-react-v2/react-dom.js
@@ -6,44 +6,43 @@ function legacyRenderSubtreeIntoContainer(element, container, callback) {
   let root = container._reactRootContainer;
   if (!root) {
     // 首次渲染
-    beginWork(element);
+    beginWork(element, container);
   }
 }
-function createNode(element, parentFiber) {
-  // vnode->fiber
-  const { type, props } = element;
-  let vnode = null;
-  if (typeof type === 'function') {
-    vnode = type.prototype.isReactComponent
-      ? updateClassComponent(element)
-      : updateFunctionComponent(element);
-  }
-  const fiber = {
-    type: type,
-    stateNode: vnode,
-    child: null,
-    sibling: null,
-    return: parentFiber,
-    props: props
-  };
-  reconcileChildren(vnode, fiber);
-  return fiber;
-}
-function reconcileChildren(vnode, parentFiber) {
-  const children = vnode?.props?.children;
+/**
+ * @desc 协调子节点
+ */
+function reconcileChildren(children, workInProgress) {
+  console.log('reconcileChildren', children);
   if (children) {
     const _children = Array.isArray(children) ? children : [children];
     let previous = null;
     for (let i = 0; i < _children.length; i++) {
-      const filber = createNode(_children[i], parentFiber);
+      let child = _children[i];
+      if (typeof child === 'string') {
+        // workInProgress.stateNode.appendChild(document.createTextNode(child));
+        console.log(workInProgress);
+        continue;
+      }
+      let newFiber = {
+        type: child.type,
+        stateNode: null,
+        child: null,
+        sibling: null,
+        return: workInProgress,
+        props: { ...child.props }
+      };
+      updateHostComponent(newFiber);
       if (i === 0) {
         // 第一个子节点
-        parentFiber.child = filber;
+        workInProgress.child = newFiber;
       }
       if (previous) {
-        previous.sibling = filber;
+        previous.sibling = newFiber;
       }
-      previous = filber;
+      previous = newFiber;
+      // console.log(workInProgress.stateNode, newFiber.stateNode);
+      // workInProgress.stateNode.appendChild(newFiber.stateNode);
     }
   }
 }
@@ -52,35 +51,76 @@ function updateClassComponent(element) {
   const instance = new type(props);
   return instance.render();
 }
-function updateHostComponent(element) {
-  const { type } = element;
-  return document.createElement(type);
-}
-function updateFunctionComponent(element) {
-  const { type, props } = element;
-  return type(props);
+function updateHostComponent(workInProgress) {
+  const { type, props } = workInProgress;
+  if (!workInProgress.stateNode) {
+    if (typeof props.children === 'string') {
+      // 文本节点
+      workInProgress.stateNode = document.createTextNode(props.children);
+    } else {
+      workInProgress.stateNode = document.createElement(type);
+    }
+  }
+  reconcileChildren(props?.children, workInProgress);
 }
 function commmitRoot() {
-  // fiber -> node
+  // console.log(rootFiber);
 }
-function beginWork(element) {
-  let current = createNode(element);
-  console.log(current);
-  // workLoop(current);
+let rootFiber = null;
+let nextUnitOfWork = null;
+function beginWork(element, container) {
+  rootFiber = nextUnitOfWork = {
+    type: element.type,
+    key: element.key,
+    props: { ...element.props },
+    child: null,
+    return: null,
+    sibling: null,
+    stateNode: container
+  };
+  workLoop(nextUnitOfWork);
 }
-
-function performUnitOfWork(workInProgress) {}
-
-function workLoop(workInProgress) {
-  requestIdleCallback(deadline => {
-    while (workInProgress && deadline.timeRemaining() > 0) {
-      workInProgress = performUnitOfWork(workInProgress);
+function updateFunctionComponent(workInProgress) {
+  const { type, props } = workInProgress;
+  const jsx = type(props);
+  if (!workInProgress.stateNode) {
+    workInProgress.stateNode = jsx;
+  }
+  if (!props.children) {
+    props.children = jsx;
+  }
+  reconcileChildren(props.children, workInProgress);
+}
+function performUnitOfWork(workInProgress) {
+  // 深度遍历生成fiber
+  const { type } = workInProgress;
+  if (typeof type === 'function') {
+    updateFunctionComponent(workInProgress);
+  } else {
+    updateHostComponent(workInProgress);
+  }
+  // 返回下一个fiber
+  if (workInProgress.child) {
+    return workInProgress.child;
+  }
+  let nextFiber = workInProgress;
+  while (nextFiber) {
+    if (workInProgress.sibling) {
+      return workInProgress.sibling;
     }
-    if (!workInProgress) {
-      commmitRoot(); // 提交阶段，渲染dom到页面
-    }
-  });
+    nextFiber = nextFiber.return;
+  }
 }
+function workLoop(deadline) {
+  // while (nextUnitOfWork && deadline.timeRemaining() > 0) {
+  while (nextUnitOfWork) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+  }
+  if (!nextUnitOfWork) {
+    commmitRoot(); // 提交阶段，渲染dom到页面
+  }
+}
+// requestIdleCallback(workLoop);
 
 export default {
   render
